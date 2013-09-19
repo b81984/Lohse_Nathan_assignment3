2fe363595f90d2d163308cf2fb8477c0ff1106c9
b81984 <lordxchris@gmail.com>
2013-09-19 12:42:04 -0500

    Initial Setup

--- rename-me.js
+++ rename-me.js
@@ -1,1 +1,21 @@
+/*Nathan Lohse
+9/19/2013
+x-COM: Iron Man*/
+
 alert("JavaScript works!");
+var totalShotsFired = 0;
+
+var approach = function()
+{
+
+}
+var combat = function()
+{
+	
+}
+var shotsFired = function(bullets) 
+{
+
+}
+
+var 
